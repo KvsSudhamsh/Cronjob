a9c1a36668bad311b9811d0dff3b6c1d7527fcb4
KvsSudhamsh <vishnu.kvs202@gmail.com>
2025-01-01 20:37:58 -0800

    gitCommit method and log status

--- updatenumber.py
+++ updatenumber.py
@@ -1,9 +1,12 @@
 import subprocess
 import os
 from datetime import datetime
+from log import logger
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
+
+log = logger(__name__)
 
 def readNumber()-> int:
     with open('number.txt', 'r') as f:
@@ -18,17 +21,23 @@ def writeNumber(n : int)-> None:
 def updateNumber(n:int)-> int:
     return n + 1
 
-def gitCommit():
-    pass
+def gitCommit()-> None:
+    subprocess.run(['git', 'add', 'number.txt'])
+    date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    commitMessage = f"Update number : {date}"
+    subprocess.run(['git', 'commit', '-m', commitMessage])
+    return None
     
 
 def main():
     try:
         number = readNumber()
+        log.info(f"number: {number}")
         number = updateNumber(number)
         writeNumber(number)
+        gitCommit()
     except Exception as e:
-        print(e)
+        log.info(f"Error:{e}")
 
 if __name__ == '__main__':
     main()
